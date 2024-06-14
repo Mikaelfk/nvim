47c722ab3cb6498343fa591d07a63cfeb7fe3e7e
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2024-06-14 23:21:41 +0200

    Add tresitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+		build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    opts = function(_, opts)
+      opts.highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      }
+    end,
+  },
+}
