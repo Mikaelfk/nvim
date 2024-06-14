0b3100894ab0785a498a59777c4f9e4e80ea07dd
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2024-06-14 23:04:46 +0200

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,46 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- luacheck: ignore
+
+-- Shorten vim.opt to opt
+local opt = vim.opt
+
+-- Disable swapfile
+opt.swapfile = false
+
+-- Disable backup
+opt.backup = false
+
+-- Disable writebackup
+opt.writebackup = false
+
+-- Color 120th column
+opt.colorcolumn = "120"
+
+-- Enable line wrap, and make it so that long lines wrap smartly
+opt.wrap = true
+opt.breakindent = true
+opt.showbreak = string.rep(" ", 3)
+opt.linebreak = true
+
+-- Set encoding to utf-8
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+-- Number of spaces that a <Tab> counts for while performing editing operations
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.tabstop = 2
+
+-- Enable incremental search
+opt.incsearch = true
+
+-- Enable Spellcheck
+opt.spell = false
+
+-- Motions like "G" also move to the first char
+opt.startofline = true
+
+opt.clipboard = "unnamedplus"
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		'akinsho/bufferline.nvim', 
+		version = "*", 
+		dependencies = 'nvim-tree/nvim-web-devicons',
+		opts = function()
+			require('bufferline').setup({
+				options = {
+					mode = 'buffers',
+					offsets = {
+						{filetype = 'NvimTree'}
+					},
+				},
+				highlights = {
+					buffer_selected = {
+						italic = false
+					},
+					indicator_selected = {
+						fg = {attribute = 'fg', highlight = 'Function'},
+						italic = false
+					}
+				}
+			})
+		end,
+	},
+}
+
+
