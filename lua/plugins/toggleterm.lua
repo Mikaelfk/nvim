990753d18a57c02e45eca9e03a45145ea13a5dc0
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2024-06-15 12:57:37 +0200

    Add toggleterm plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    {
+      'akinsho/toggleterm.nvim', 
+      version = "*", 
+      opts = function()
+        require("toggleterm").setup{
+          open_mapping = '<C-g>',
+            direction = 'horizontal',
+            shade_terminals = true
+        }
+      end,
+    }
+  },
+}
