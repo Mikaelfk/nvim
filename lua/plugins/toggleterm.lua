313d9161cb80794effacf6a708f9ea88f5a0f10c
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2024-06-15 13:16:19 +0200

    Formatting changes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,15 +1,13 @@
 return {
   {
-    {
-      'akinsho/toggleterm.nvim', 
-      version = "*", 
-      opts = function()
-        require("toggleterm").setup{
-          open_mapping = '<C-g>',
-            direction = 'horizontal',
-            shade_terminals = true
-        }
-      end,
-    }
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    opts = function()
+      require("toggleterm").setup{
+        open_mapping = '<C-g>',
+          direction = 'horizontal',
+          shade_terminals = true
+      }
+    end,
   },
 }
