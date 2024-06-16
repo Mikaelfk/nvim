91d2c3ecacb7a68efca46cadedf84ee24b74ddab
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2024-06-16 12:51:54 +0200

    Use telescope UI for harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,9 +7,28 @@ return {
       local harpoon = require("harpoon")
 
       harpoon:setup()
+      -- basic telescope configuration
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers").new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        }):find()
+      end
+
+      vim.keymap.set("n", "<C-r>", function() toggle_telescope(harpoon:list()) end,
+      { desc = "Open harpoon window" })
 
       vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<C-r>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
       vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
@@ -19,6 +38,10 @@ return {
       vim.keymap.set("n", "<leader><C-2>", function() harpoon:list():replace_at(2) end)
       vim.keymap.set("n", "<leader><C-3>", function() harpoon:list():replace_at(3) end)
       vim.keymap.set("n", "<leader><C-4>", function() harpoon:list():replace_at(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-K>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-J>", function() harpoon:list():next() end)
     end,
   },
 }
